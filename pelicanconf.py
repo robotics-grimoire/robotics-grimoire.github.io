c63b32803467ccb5d10c886d566e94290f765a7f
John T Hill IV <jhill515@gmail.com>
2019-06-30 15:23:35 -0400

    Minor fixes (check diff)

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,7 +14,13 @@ TIMEZONE = 'America/New_York'
 DEFAULT_LANG = 'en'
 
 DISPLAY_CATEGORIES_ON_MENU = False
-DISPLAY_PAGES_ON_MENU = True
+DISPLAY_PAGES_ON_MENU = False
+
+MENUITEMS = (
+    ('Blog', 'https://robotics-grimoire.github.io'),
+    ('About', '/pages/about.html'),
+    ('References', '/pages/references.html'),
+    )
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
