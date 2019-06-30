ad152b718c08052acf1146116721d77c7c015c5b
John T Hill IV <jhill515@gmail.com>
2019-06-30 15:53:17 -0400

    Added plugins to pelican config file

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+import os
 
 AUTHOR = 'robotics-grimoire'
 SITENAME = 'The Robotics Grimoire'
@@ -22,6 +23,13 @@ MENUITEMS = (
     ('References', '/pages/references.html'),
     )
 
+PLUGIN_PATHS = [os.environ['HOME']+'/Projects/pelican-plugins']
+PLUGINS = [
+    'better_codeblock_line_numbering',
+    'render_math',
+    'related_posts',
+    ]
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -30,14 +38,16 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('GitHub', 'http://github.com/'),
-        )
+LINKS = (
+    ('Pelican', 'http://getpelican.com/'),
+    ('GitHub', 'http://github.com/'),
+    )
 
 # Social widget
-SOCIAL = (('LinkedIn', 'https://www.linkedin.com/jhill515'),
-          ('Stack Overflow', 'https://stackoverflow.com/users/story/2370362'),
-         )
+SOCIAL = (
+    ('LinkedIn', 'https://www.linkedin.com/jhill515'),
+    ('Stack Overflow', 'https://stackoverflow.com/users/story/2370362'),
+    )
 
 DEFAULT_PAGINATION = 10
 
